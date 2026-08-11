dec34edb20f3144c263ae1f6f78e5270767c2f0d
hexuhong <hexuhong@ebankunion.com>
2026-08-11 13:53:18 +0800

    fix施肥
    (cherry picked from commit 36f011538c1458b84854c7fce3a5295aca1dd46b)

--- autoJsPro/tb2/iconDefines.js
+++ autoJsPro/tb2/iconDefines.js
@@ -56,7 +56,7 @@ module.exports = function (runtime, scope) {
 
         // ---- 施肥 ----
         "shifei": {
-            dy: 200,
+            dy: 20,
             threshold: 0.78,
             region: [
                 Math.floor(device.width * 0.2), Math.floor(device.height * 0.2),
